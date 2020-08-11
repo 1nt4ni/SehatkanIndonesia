e6e1cfaf7cdb52bc4a617efcbfe3ab6fe0ce561e
1nt4ni <intan_dewi_28rpl@student.smktelkom-mlg.sch.id>
2020-08-11 14:30:59 +0700

    Initial commit

---
+++ app/src/main/java/com/dicoding/picodiploma/sehatkanindonesia/MoveActivity.java
@@ -0,0 +1,16 @@
+package com.dicoding.picodiploma.sehatkanindonesia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dicoding.picodiploma.sehatkanindonesia.R;
+
+public class MoveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move);
+    }
+}
